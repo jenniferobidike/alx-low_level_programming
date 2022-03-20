07dfa1c0840042bf3d119e6c339967095d8e9a3a
jenniferobidike <thejoysofexistence@gmail.com>
2022-03-20 15:53:49 -0700

    4-pint_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+			putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
