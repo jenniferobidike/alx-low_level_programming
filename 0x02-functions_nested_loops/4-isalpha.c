3ee11fb4f5d4ea8621bb32820f27c2043ae8310e
jenniferobidike <thejoysofexistence@gmail.com>
2022-03-24 05:05:48 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - chceks if c is a lowercase or uppercase letter
+ * @c: letter to be checked
+ *
+ * Return: 1 if c is lowercase or uppercase letter, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
