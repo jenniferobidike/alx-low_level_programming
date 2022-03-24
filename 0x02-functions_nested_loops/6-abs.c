2d474360f02620d6ae381c6b8646fb5884e3df5b
jenniferobidike <thejoysofexistence@gmail.com>
2022-03-24 05:44:47 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @ab: character to be checked
+ *
+ * Return: Absolute value of an integer
+ */
+
+int _abs(int ab)
+{
+	if (ab >= 0)
+		return (ab);
+	else
+		return (-ab);
+}
