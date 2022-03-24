ca7872751f7d45a683cd00691aee416c78742dbf
jenniferobidike <thejoysofexistence@gmail.com>
2022-03-24 04:00:03 -0700

    2-print_alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lower case letters x10
+ */
+
+void print_alphabet_x10(void)
+{
+
+	char alpha;
+	int count;
+
+	while (count++ <= 9)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+
+		_putchar('\n');
+	}
+}
+
