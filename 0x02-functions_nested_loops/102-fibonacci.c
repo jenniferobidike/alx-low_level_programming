268a348574fb8d44cb9556c4b66de86da0ce81aa
root <thejoysofexistence@gmail.com>
2022-06-11 16:37:24 -0700

    102-fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int a;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (a = 0; a < 50; a++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (a == 49)
+			printf("\n");
+
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
