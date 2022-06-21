eaa9be56114559f24f44c58604bae691b7cf464b
root <thejoysofexistence@gmail.com>
2022-06-21 01:18:17 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry point
+ * @s: the string to be searched
+ * @accept: the set of bytes to be searched for
+ * Return: Always 0 (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (s[i] == accept[n])
+				return (s + i);
+		}
+	}
+
+	return (NULL);
+}
