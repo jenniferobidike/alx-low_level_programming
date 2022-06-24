2f13df30ba0ae927020a4dfcf86af5b551e8a4b0
root <thejoysofexistence@gmail.com>
2022-06-23 21:04:45 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,62 +1,32 @@
 #include "main.h"
 
-int find_strlen(char *s);
-int check_palindrome(char *s, int len, int index);
-int is_palindrome(char *s);
-
 /**
- * find_strlen - Returns the length of a string.
- * @s: The string to be measured.
+ * wildcmp - compares two strings and returns 1 if the strings
+ * can be considered identical, otherwise return 0.
+ * @s1: string to compare to
+ * @s2: string with wild character
  *
- * Return: The length of the string.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
-int find_strlen(char *s)
+int wildcmp(char *s1, char *s2)
 {
-	int len = 0;
 
-	if (*(s + len))
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
 	{
-		len++;
-		len += find_strlen(s + len);
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+			return (0);
+
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
 	}
-	return (len);
-}
 
-/**
- * check_palindrome - Checks if a string is a palindrome.
- * @s: The string to be checked.
- * @len: The length of s.
- * @index: The index of the string to be checked.
- *
- * Return: If the string is a palindrome - 1.
- *         If the string is not a palindrome - 0.
- */
-
-int check_palindrome(char *s, int len, int index)
-{
-	if (s[index] == s[len / 2])
-		return (1);
-
-	if (s[index] == s[len - index - 1])
-		return (check_palindrome(s, len, index + 1));
 	return (0);
-}
-
-/**
- * is_palindrome - Checks if a string is a palindrome.
- * @s: The string to be checked.
- *
- * Return: If the string is a palindrome - 1.
- *         If the string is not a palindrome - 0.
- */
-
-int is_palindrome(char *s)
-{
-	int index = 0;
-	int len = find_strlen(s);
-
-	if (!(*s))
-		return (1);
-	return (check_palindrome(s, len, index));
 }
