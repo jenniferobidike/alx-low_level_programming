efbae1b96d79fa3d4d31d9e979c94a99fceb1723
root <thejoysofexistence@gmail.com>
2022-06-29 11:11:23 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,45 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * alloc_grid - nested loop to make grid
- * @width: width input
- * @height: height input
- * Return: pointer to 2 dimensional array
+ * alloc_grid - Returns a pointer to a 2-dimensional array
+ * @width: The width of the 2-dimensional array.
+ * @height: The height of the 2-dimensional array.
+ * Return: If width <= 0, height <= 0, or the function fails - NULL.
  */
 
 int **alloc_grid(int width, int height)
 {
 
 	int **iarray;
-	int i, n;
+	int hgt_index, wid_index;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	iarray = malloc(sizeof(int) * height);
+	iarray = malloc(sizeof(int *) * height);
 
 	if (iarray == NULL)
-	{
-		free(iarray);
 		return (NULL);
-	}
 
-	for (i = 0; i < height; i++)
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
-		iarray[i] = malloc(sizeof(int) * width);
-		if (iarray == NULL)
+		iarray[hgt_index] = malloc(sizeof(int) * width);
+
+		if (iarray[hgt_index] == NULL)
 		{
+			for (; hgt_index >= 0; hgt_index--)
+				free(iarray[hgt_index]);
+
 			free(iarray);
 			return (NULL);
 		}
+	}
 
-		for (n = 0; n < width; n++)
-		{
-			iarray[i][n] = 0;
-		}
-
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		for (wid_index = 0; wid_index < width; wid_index++)
+			iarray[hgt_index][wid_index] = 0;
 	}
 	return (iarray);
 
