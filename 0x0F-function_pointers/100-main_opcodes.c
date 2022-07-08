f071f97c2913262adb1cb459dea076baebd11b37
root <thejoysofexistence@gmail.com>
2022-07-07 21:55:28 -0700

    opcodes correction

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,40 +2,34 @@
 #include <stdlib.h>
 
 /**
- * main - prints opcode of own main function
- * @argc: argument count
- * @argv: array of arguments
- * Return: 1 or 2 on fail, 0 on success
+ * main - program that prints the opcodes of its own main function
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ *
+ * Return: 0 on succes, 1 on argv != 2, 2 on negative bytes in argv
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int bytes, i;
-	unsigned char *func_ptr;
+
+	int i, bytes;
+	char *jen;
 
 	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
+		printf("Error\n"), exit(1);
 
 	bytes = atoi(argv[1]);
 
 	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
+		printf("Error\n"), exit(2);
 
-	func_ptr = (unsigned char *)main;
-	i = 0;
+	jen = (char *)main;
 
-	if (bytes > 0)
-	{
-		while (i < (bytes - 1))
-			printf("%02hhx ", func_ptr[i++]);
-		printf("%hhx\n", func_ptr[i]);
-	}
+	for (i = 0; i < bytes - 1; i++)
+		printf("%02hhx ", jen[i]);
+
+	printf("%02hhx\n", jen[i]);
 
 	return (0);
+
 }
